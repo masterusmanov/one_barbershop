7bcfaf835e3eb9a8ce599f930e05bb6183e78eb9
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-10-16 11:55:55 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-var ElOpenBtn = document.querySelector(".site-header__burger");
-var ElCloseBtn = document.querySelector(".site-header-close");
-var siteNavbar = document.querySelector(".site-nav__wrapper");
 var currentYear = document.querySelector("#currentYear");
 
 currentYear.innerHTML = new Date().getFullYear()
-ElOpenBtn.addEventListener("click", function () {
-  siteNavbar.classList.add("shownav");
-});
-ElCloseBtn.addEventListener("click", function () {
-  siteNavbar.classList.remove("shownav");
-});
