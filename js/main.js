a7b218fe4d87651b88e701bb622753a7d8122035
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-10-25 15:27:57 +0500

    update

--- js/main.js
+++ js/main.js
@@ -29,8 +29,3 @@ contentEng.style.display = 'none';
             contentRus.style.display = 'none';
         }
     });
-
-
-if (window.location.pathname.endsWith('/')) {
-    window.location.pathname = window.location.pathname.slice(0, -1);
-}
