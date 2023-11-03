5e5c4fda11e04be80a4db72e98c990d2229d19bd
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-11-03 18:07:05 +0500

    update

--- js/main.js
+++ js/main.js
@@ -4,28 +4,30 @@ const contentRus = document.getElementById('content-rus');
 const contentUz = document.getElementById('content-uzb');
 const contentEng = document.getElementById('content-eng');
 
+       
+
 currentYear.innerHTML = new Date().getFullYear()
 
 contentUz.style.display = 'none';
 contentEng.style.display = 'none';
 
-    // Add an event listener to the language selector
-    languageSelector.addEventListener('change', function () {
-        const selectedLanguage = languageSelector.value;
-            
 
-        // Show/hide content based on the selected language
-        if (selectedLanguage === 'rus') {
-            contentRus.style.display = 'block';
-            contentUz.style.display = 'none';
-            contentEng.style.display = 'none';
-        } else if (selectedLanguage === 'uzb') {
-            contentUz.style.display = 'block';
-            contentRus.style.display = 'none';
-            contentEng.style.display = 'none';
-        } else if( selectedLanguage === 'eng'){
-            contentEng.style.display = 'block';
-            contentUz.style.display = 'none';
-            contentRus.style.display = 'none';
-        }
-    });
+languageSelector.addEventListener('change', function () {
+    const selectedLanguage = languageSelector.value;
+        
+
+    // Show/hide content based on the selected language
+    if (selectedLanguage === 'rus') {
+        contentRus.style.display = 'block';
+        contentUz.style.display = 'none';
+        contentEng.style.display = 'none';
+    } else if (selectedLanguage === 'uzb') {
+        contentUz.style.display = 'block';
+        contentRus.style.display = 'none';
+        contentEng.style.display = 'none';
+    } else if( selectedLanguage === 'eng'){
+        contentEng.style.display = 'block';
+        contentUz.style.display = 'none';
+        contentRus.style.display = 'none';
+    }
+});
